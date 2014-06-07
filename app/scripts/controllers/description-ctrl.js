8b583cc0ebe7088a48dcf0b1fb6fd66b6d09776c
Sahil Chachan <chachan.sahil@gmail.com>
2014-06-07 17:13:09 +0530

    Added controllers for 3 tabs

---
+++ app/scripts/controllers/description-ctrl.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('sportbookingAssignmentApp')
+	.controller('DescriptionCtrl',function(){
+
+});
---
+++ app/scripts/controllers/movies-ctrl.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('sportbookingAssignmentApp')
+	.controller('MoviesCtrl',function(){
+
+});
